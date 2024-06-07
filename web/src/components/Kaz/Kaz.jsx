7bb8b4d109f18b4bfa88d2649b3c47ee35313a45
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-06-07 10:02:45 +0300

    feat: updated assets and adjusted style for slots

--- web/src/components/Kaz/Kaz.jsx
+++ web/src/components/Kaz/Kaz.jsx
@@ -1,13 +1,29 @@
+import { useRef } from "react";
+
 export function Kaz() {
+  const reel1 = useRef();
+  const reel2 = useRef();
+  const reel3 = useRef();
+
   return (
     <div className="min-w-dvw min-h-dvh flex justify-center items-center">
       <div id="centered-holder" className="flex flex-col items-center">
         <img src="images/kaz/try-your-luck.png" width="45%" />
-        <img
-          src="images/kaz/slot-full-beta.png"
-          width="55%"
-          className="shadow-black"
-        />
+        <div
+          id="slot-machine-holder"
+          className="w-dvw h-fit flex flex-col items-center"
+        >
+          <img
+            src="images/kaz/slot-full-beta.png"
+            width="520px"
+            className="shadow-black z-20 my-8"
+          />
+          <div id="reels-holder" className="w-[29.5rem] absolute mt-[15em] z-50 flex justify-around">
+            <div className="reel" ref={reel1}></div>
+            <div className="reel" ref={reel2}></div>
+            <div className="reel" ref={reel3}></div>
+          </div>
+        </div>
         <button className="spin-btn">SPIN!</button>
         <img src="images/kaz/get-3-wins-in-a-row.png" width="50%" />
       </div>
