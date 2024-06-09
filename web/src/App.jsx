ae559259abc23239a71ad296ece476589ad34153
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-06-09 16:10:10 +0300

    feat: establish routing between all pages

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,15 +1,30 @@
-import { Kaz } from './components/Kaz/Kaz.jsx'
-import { WelcomePage } from './components/WelcomePage/WelcomePage.jsx'
-import { NavBar } from './components/WelcomePage/NavBar/NavBar.jsx'
+import {Kaz} from './components/Kaz/Kaz.jsx'
+import {WelcomePage} from './components/WelcomePage/WelcomePage.jsx'
+import {NavBar} from './components/WelcomePage/NavBar/NavBar.jsx'
+import {Route, Routes} from "react-router-dom";
+import {Roadmap} from "./components/Roadmap/Roadmap.jsx";
+import AboutUs from "./components/AboutUs/AboutUs.jsx";
+import Garage from "./components/Garage/Garage.jsx";
+import Merch from "./components/Merch/Merch.jsx";
 
 function App() {
-    return (
-        <main className="max-w-dvw h-full bg-[#2E2E2E]">
+    return (<main className="max-w-dvw h-full bg-[#2E2E2E]">
             {/* lighter gamma: #414141 */}
-            <NavBar />
-            <WelcomePage />
-            <Kaz />
+            <NavBar/>
+            <Routes>
+                <Route path="/" element={
+                    <div className={"max-w-dvw h-full"}>
+                        <WelcomePage/>
+                        <Kaz/>
+                    </div>}
+                />
+                <Route path="/roadmap" element={<Roadmap/>}/>
+                <Route path="/about-us" element={<AboutUs/>}/>
+                <Route path="/garage" element={<Garage/>}/>
+                <Route path="/merch" element={<Merch/>}/>
+            </Routes>
         </main>
+
     )
 }
 
--- web/src/components/WelcomePage/NavBar/NavBar.jsx
+++ web/src/components/WelcomePage/NavBar/NavBar.jsx
@@ -1,4 +1,7 @@
 import Lottie from 'lottie-react'
+import {Link, Route, Routes} from "react-router-dom";
+import {Roadmap} from "../../Roadmap/Roadmap.jsx";
+import AboutUs from "../../AboutUs/AboutUs.jsx";
 
 export function NavBar() {
     return (
@@ -9,26 +12,37 @@ export function NavBar() {
                     className="flex w-fit items-end justify-center"
                 >
                     <div className="nav-btn-holder">
-                        <button className="nav-btn">ROADMAP</button>
-                        <button className="nav-btn">ABOUT US</button>
+                        <Link to="/roadmap">
+                            <button className="nav-btn">ROADMAP</button>
+                        </Link>
+                        <Link to="/about-us">
+                            <button className="nav-btn">ABOUT US</button>
+                        </Link>
                     </div>
                     <div
                         id="wheel-holder"
                         className="relative mx-12 mb-[-1.3%] mt-[1%] w-[7%]"
                     >
-                        <Lottie
-                            path={'lotties/welcomePage/low-nonstop-wheel.json'}
-                            loop={true}
-                            autoplay={true}
-                        />
+                        <Link to={"/"}>
+                            <Lottie
+                                path={'lotties/welcomePage/low-nonstop-wheel.json'}
+                                loop={true}
+                                autoplay={true}
+                            />
+                        </Link>
                     </div>
                     <div className="nav-btn-holder">
-                        <button className="nav-btn">GARAGE</button>
-                        <button className="nav-btn">OUR MERCH</button>
+
+                        <Link to="/garage">
+                            <button className="nav-btn">GARAGE</button>
+                        </Link>
+                        <Link to="/merch">
+                            <button className="nav-btn">OUR MERCH</button>
+                        </Link>
                     </div>
                 </div>
             </header>
-            <img src="images/welcomePage/nav-bar-hr.png" width={'67%'} />
+            <img src="images/welcomePage/nav-bar-hr.png" width={'67%'}/>
         </div>
     )
 }
