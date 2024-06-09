376b1681e9cf19a85363510362a14cd945e2df72
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-06-09 16:46:49 +0300

    feat: switch animation between website panels

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,28 +1,40 @@
 import {Kaz} from './components/Kaz/Kaz.jsx'
 import {WelcomePage} from './components/WelcomePage/WelcomePage.jsx'
 import {NavBar} from './components/WelcomePage/NavBar/NavBar.jsx'
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useLocation} from "react-router-dom";
 import {Roadmap} from "./components/Roadmap/Roadmap.jsx";
 import AboutUs from "./components/AboutUs/AboutUs.jsx";
 import Garage from "./components/Garage/Garage.jsx";
 import Merch from "./components/Merch/Merch.jsx";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
+import "./index.css";
 
 function App() {
-    return (<main className="max-w-dvw h-full bg-[#2E2E2E]">
+    const location = useLocation();
+
+    return (<main className="max-w-dvw h-full bg-[#2E2E2E] overflow-hidden">
             {/* lighter gamma: #414141 */}
             <NavBar/>
-            <Routes>
-                <Route path="/" element={
-                    <div className={"max-w-dvw h-full"}>
-                        <WelcomePage/>
-                        <Kaz/>
-                    </div>}
-                />
-                <Route path="/roadmap" element={<Roadmap/>}/>
-                <Route path="/about-us" element={<AboutUs/>}/>
-                <Route path="/garage" element={<Garage/>}/>
-                <Route path="/merch" element={<Merch/>}/>
-            </Routes>
+            <TransitionGroup>
+                <CSSTransition
+                    key={location.key}
+                    timeout={300}
+                    classNames="fade"
+                >
+                    <Routes>
+                        <Route path="/" element={
+                            <div className={"max-w-dvw h-full"}>
+                                <WelcomePage/>
+                                <Kaz/>
+                            </div>}
+                        />
+                        <Route path="/roadmap" element={<Roadmap/>}/>
+                        <Route path="/about-us" element={<AboutUs/>}/>
+                        <Route path="/garage" element={<Garage/>}/>
+                        <Route path="/merch" element={<Merch/>}/>
+                    </Routes>
+                </CSSTransition>
+            </TransitionGroup>
         </main>
 
     )
