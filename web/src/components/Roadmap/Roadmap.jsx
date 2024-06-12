f7ab348987c3169c3cfc5759973ebea00ae24147
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-06-12 15:45:35 +0300

    feat: add animated driving car to roadmap

--- web/src/components/Roadmap/Roadmap.jsx
+++ web/src/components/Roadmap/Roadmap.jsx
@@ -1,16 +1,25 @@
-import Lottie from "lottie-react";
-
+import Lottie from 'lottie-react'
 export default function Roadmap() {
-    return <div className="max-w-dvw h-dvh flex flex-col items-center justify-center">
-        <img src={"images/roadmap/road.png"} className="mt-10 z-0 w-full"/>
-        <div id="road-signs-and-text-holder" className="z-20 absolute w-dvw flex justify-end items-end">
-            <img src={"images/roadmap/roadmap-signs-and-text.png"} className="w-80% mr-[2%]"/>
+    return (
+        <div className="flex h-dvh w-dvw flex-col items-center justify-center">
+            <img src={'images/roadmap/road.png'} className="z-0 mt-10 w-full" />
+
+            <Lottie
+                className={'l-0 absolute mt-[-2%] w-full'}
+                path={'lotties/roadmap/car-roadmap.json'}
+                loop={true}
+                autoplay={true}
+            />
+
+            <div
+                id="road-signs-and-text-holder"
+                className="absolute z-20 flex w-full items-end justify-end"
+            >
+                <img
+                    src={'images/roadmap/roadmap-signs-and-text.png'}
+                    className="mr-[2%] max-[1700px]:w-[80%]"
+                />
+            </div>
         </div>
-        <Lottie
-            className={'absolute right-[5%] top-[10%] w-[50%]'}
-            // path={'lotties/roadmap/roadmap-driving-car.json'}
-            loop={false}
-            autoplay={true}
-        />
-    </div>
+    )
 }
