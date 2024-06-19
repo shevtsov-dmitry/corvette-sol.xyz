288565136b69b6a504dea038761a17c5393574b2
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-06-19 19:46:24 +0300

    feat: hello message to user.

--- botchecker/main.py
+++ botchecker/main.py
@@ -1,16 +1,37 @@
+import logging
 import os
 
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import ReplyKeyboardMarkup, Update
+from telegram.ext import ApplicationBuilder
+from telegram.ext import (
+    CommandHandler,
+    ContextTypes,
+)
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+bot_token = os.getenv('BOT_TOKEN')
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-bot_token = os.getenv('BOT_TOKEN')
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        f"Hi, dear {update.effective_user.first_name}! I will check your wallet real quick.\n" +
+        "Send it to me to get verification ✅."
+    )
+
 
 app = ApplicationBuilder().token(bot_token).build()
 
 app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
