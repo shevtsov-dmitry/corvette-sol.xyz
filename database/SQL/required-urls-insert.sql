a66b0c988ccae6c9eaf9910ded02d58f61da627f
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-09-10 12:27:27 +0300

    refactor: switch default db to postgresql with envs

---
+++ database/SQL/required-urls-insert.sql
@@ -0,0 +1,8 @@
+-- In the corvette database execute
+insert into db.Url (id, website, url)
+values  (1, 'twitter', 'https://twitter.com/corvetteCar'),
+        (2, 'telegram', 'https://twitter.com/corvetteCarfds'),
+        (3, 'pumpfun', 'https://pumpfun.com'),
+        (4, 'tensor', 'https://tensor.com/'),
+        (5, 'site', 'https://twitter.com/corvetteCare'),
+        (6, 'pumpfun_bonding_curve', '55');
--- server/src/main/java/com/corvette/corvette/CorvetteService.java
+++ server/src/main/java/com/corvette/corvette/CorvetteService.java
@@ -1,11 +1,14 @@
 package com.corvette.corvette;
 
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,6 +18,7 @@ import java.util.*;
 @Service
 public class CorvetteService {
     private final ResourceLoader resourceLoader;
+    private final Logger log = LoggerFactory.getLogger(CorvetteService.class);
 
     @Value("${custom.env.IMAGES_STORAGE_PATH}")
     private String IMAGES_STORAGE_PATH;
@@ -23,6 +27,16 @@ public class CorvetteService {
     @PostConstruct
     public void init() {
         this.dirPath = Paths.get(IMAGES_STORAGE_PATH);
+        if (!Files.exists(dirPath)) {
+            try {
+                log.warn("Couldn't find the image storage directory, creating it now... " +
+                        "\nYou need to contain carmodel images by path {}", "${HOME}/Pictures/corvette/carmodels");
+                Files.createDirectories(dirPath);
+            } catch (IOException e) {
+                log.error("Error while trying to initialize creating the directory: {}", e.getMessage());
+                System.exit(1);
+            }
+        }
     }
 
     public CorvetteService(ResourceLoader resourceLoader) {
